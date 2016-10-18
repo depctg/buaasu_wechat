28298e97888dd1f046da4f89f99803be1a46702b
depctg <depctg@gmail.com>
2016-10-18 16:27:37 +0800

    bugfix

--- app/controllers/wechats_controller.rb
+++ app/controllers/wechats_controller.rb
@@ -43,78 +43,89 @@ class WechatsController < ApplicationController
 
 
       user = User.find_by(open_id: request[:FromUserName])
-      if user.nil?
-        # create User here
-        user = User.new
-        user.open_id = request[:FromUserName]
-        user.remote_avatar_url = Wechat.api.user(request[:FromUserName])['headimgurl']
-      elsif user.avatar.file.nil?
-        user.remote_avatar_url = Wechat.api.user(request[:FromUserName])['headimgurl']
-      end
-
-      if user.sign_record.nil?
-        user.sign_record = SignRecord.new
-        user.sign_record.days = []
-        user.sign_record.day = 0
-      end
-
-      # if is valid
-      user_status = false
-      user_msg = nil
-
-      now_t = Time.now
-      now_date = now_t.strftime('%Y-%m-%d')
-      # hardcode this
-      start_t = "#{now_date} 05:00:00 +0800".to_time
-      end_t = "#{now_date} 20:00:00 +0800".to_time
-      if start_t <= now_t && now_t < end_t
-        lastdate = (Time.now - 24.hours).strftime('%Y-%m-%d')
-        if not user.sign_record.last_sign_time
-          user_status = true
-          user.sign_record.days ||= []
-          user.sign_record.days << now_t
-          user.sign_record.day = 1
-          user.sign_record.last_sign_time = now_t
-        elsif user.sign_record.last_sign_time > "#{now_date} 00:00:00 +0800".to_time
-          user_status = false
-          user_msg = "您今天已经签过到了"
-        elsif user.sign_record.last_sign_time < "#{lastdate} 00:00:00 +0800".to_time
-          user_status = true
-          user.sign_record.days << Time.now
-          user.sign_record.day = 1
-          user.sign_record.last_sign_time = now_t
-        else
-          user_status = true
-          user.sign_record.last_sign_time = now_t
-          user.sign_record.days << Time.now
-          user.sign_record.day += 1
+      user.with_lock do
+        if user.nil?
+          # create User here
+          user = User.new
+          user.open_id = request[:FromUserName]
+          user.remote_avatar_url = Wechat.api.user(request[:FromUserName])['headimgurl']
+        elsif user.avatar.file.nil?
+          user.remote_avatar_url = Wechat.api.user(request[:FromUserName])['headimgurl']
         end
-        user.save
-      else
+
+        if user.sign_record.nil?
+          user.sign_record = SignRecord.new
+          user.sign_record.days = []
+          user.sign_record.day = 0
+        end
+
+        # if is valid
         user_status = false
-        user_msg = "现在不在签到时间。"
-      end
+        user_msg = nil
 
-      # gen picture here
-      if user_status
-        Rails.cache.write request[:FromUserName], false
-        templates = Dir.glob(File.join('public', 'uploads', 'gmtemplates', '*.jpg'))
-        templates.select! {|f| f.include?(now_date)}
-        media_id = temp_image(gen_picture(user, template: templates.sample))
-        msg = {
-            touser: request[:FromUserName],
-            msgtype: "image",
-            image:
-            {
-                       media_id: media_id
-            }
-        }
-        Wechat.api.custom_message_send msg
-        Rails.cache.delete request[:FromUserName]
-      else
-        request.reply.text user_msg unless Rails.cache.exist? request[:FromUserName]
-      end
+        now_t = Time.now
+        now_date = now_t.strftime('%Y-%m-%d')
+        # hardcode this
+        start_t = "#{now_date} 05:00:00 +0800".to_time
+        end_t = "#{now_date} 20:00:00 +0800".to_time
+        if start_t <= now_t && now_t < end_t
+          lastdate = (Time.now - 24.hours).strftime('%Y-%m-%d')
+          if not user.sign_record.last_sign_time
+            user_status = true
+            user.sign_record.days ||= []
+            user.sign_record.days << now_t
+            user.sign_record.day = 1
+            user.sign_record.last_sign_time = now_t
+          elsif user.sign_record.last_sign_time > "#{now_date} 00:00:00 +0800".to_time
+            user_status = false
+            user_msg = "您今天已经签过到了"
+          elsif user.sign_record.last_sign_time < "#{lastdate} 00:00:00 +0800".to_time
+            user_status = true
+            user.sign_record.days << Time.now
+            user.sign_record.day = 1
+            user.sign_record.last_sign_time = now_t
+          else
+            user_status = true
+            user.sign_record.last_sign_time = now_t
+            user.sign_record.days << Time.now
+            user.sign_record.day += 1
+          end
+          user.save
+        else
+          user_status = false
+          user_msg = "现在不在签到时间。"
+        end
 
+        # gen picture here
+        if user_status
+          Rails.cache.write request[:FromUserName], false
+          templates = Dir.glob(File.join('public', 'uploads', 'gmtemplates', '*.jpg'))
+          templates.select! {|f| f.include?(now_date)}
+          media_id = temp_image(gen_picture(user, template: templates.sample))
+          msg_text = {
+              touser: request[:FromUserName],
+              msgtype: "image",
+              text:
+              {
+                         content: '早安~'
+              }
+          }
+          msg = {
+              touser: request[:FromUserName],
+              msgtype: "image",
+              image:
+              {
+                         media_id: media_id
+              }
+          }
+          Wechat.api.custom_message_send msg_text
+          Wechat.api.custom_message_send msg
+          Rails.cache.delete request[:FromUserName]
+        else
+          request.reply.text user_msg unless Rails.cache.exist? request[:FromUserName]
+        end
+
+      end
   end
 
   on :text, with: /dcache/ do |request|
