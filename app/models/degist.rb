d92ad2367ab63fd824ca11fe2dfc4d226f1a0166
depctg <depctg@gmail.com>
2016-11-28 22:41:52 +0800

    bugfix

--- app/models/degist.rb
+++ app/models/degist.rb
@@ -1,8 +1,8 @@
 class Degist < ApplicationRecord
   belongs_to :user
 
-  validates :subject, presence: true
-  validates :class, presence: true
+  # validates :subject, presence: true
+  # validates :class, presence: true
 
   def Degist.count_by(subject, options = {})
     if options[:class]
